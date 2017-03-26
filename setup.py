8345ee862400af515fce0978eb4138b4a9dc8e58
scottherlihy <scottherlihy1@gmail.com>
2017-03-26 11:26:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,10 +24,9 @@ class CustomSdist(sdist):
 
     @staticmethod
     def CopyChronosSourceFiles():
-        chronosSourcePath = os.path.join('Contracts', 'Chronos')
+        chronosSourcePath = os.path.join('..', 'Contracts', 'Chronos')
         chronosDestinationPath = os.path.join('Chronos', 'ChronosScripts')
-        if not os.path.isdir(chronosDestinationPath):
-            os.mkdir(chronosDestinationPath)
+        os.mkdir(chronosDestinationPath)
         buildList = []
         for aggregate in [path for path in os.listdir(chronosSourcePath) if path.endswith('.py')]:
             buildList.append(aggregate[:-3])
@@ -36,22 +35,18 @@ class CustomSdist(sdist):
             shutil.copyfile(os.path.join(chronosSourcePath, proto), os.path.join(chronosDestinationPath, proto))
         with open(os.path.join('Chronos', 'buildList.txt'), 'w') as buildListFile:
             cPickle.dump(buildList, buildListFile)
-        chronosProtoPath = os.path.join('Chronos', '')
-        if os.path.isfile('Chronos/Chronos.proto'):
-            os.system("protoc -I={} --python_out={} Chronos/Chronos.proto".format(chronosProtoPath, chronosProtoPath))
 
 
 
 setup(name='Chronos',
       version='1.0.0',
-      author='Belvedere Trading',
-      author_email='ChronosES@belvederetrading.com',
+      author='Belvedere',
+      author_email='Team_Brown_Tech@belvederetrading.com',
       cmdclass={'sdist': CustomSdist},
       packages=find_packages(),
-      package_data={'Chronos': ['ChronosScripts/*', 'initd/*', 'bin/*', 'files/*', 'buildList.txt', 'Chronos.proto']},
+      package_data={'Chronos': ['ChronosScripts/*', 'initd/*', 'bin/*', 'files/*', 'buildList.txt']},
       url='http://pypi:28080/simple/Chronos/',
       description='A distributed event sourcing framework',
       long_description=open('README.rst').read(),
-      scripts=['Chronos/bin/chronos_deploy', 'Chronos/bin/chronoses'],
       install_requires=require_packages,
       tests_require=['mock'])
